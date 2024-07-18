dcdf66e9492e3c5dd4ba7e8a6cd4513b0d132135
MIALIS123 <lispalaciosarostegui@gmail.com>
2024-07-18 13:22:56 -0600

    Feat-Declarar Objeto auto

---
+++ JS/JavaScript-2.js
@@ -0,0 +1,32 @@
+let países = ["CR", "USA", "PA"]
+let frutas = ["manzana","pera","uva"] 
+// Tres elementos en un Array
+// Se posicionan desde cero, CR es 0 , USA es 1 y PA es 2.
+//Siempre hay números positivos. No negativos.
+//Para visualizar un país en especifico en este caso, tienes que declarar el numero con llaves cuadradas.
+console.log(países[1])
+console.log(países[2])
+
+let estudiante ={
+    Nombre: "Pedro",
+    Edad: 20,
+    EstadoCivil: true,
+    Materias: ["Progra1", "Base de Datos", "Calculo"]
+}
+
+console.log(estudiante)
+console.log(estudiante.Nombre)
+console.log(estudiante.Materias[2])
+
+//Declarar el siguiente objeto : auto , 4 propiedades : string , int ,bool y array.
+//Mostrar el Objeto completo.
+
+let auto = {
+    Marca: "Mini Cooper",
+    Año: 1999,
+    CarrodeCarreras: true,
+    Colores: ["Negro", "Vino", "Gris"]
+
+}
+
+console.log(auto)
