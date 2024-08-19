2c3c98cda0d42b49fce0bf7be52f3b1b0bbf0e7d
MIALIS123 <lispalaciosarostegui@gmail.com>
2024-08-19 01:12:33 -0600

    Ft-Arreglo de objetos Heroes

---
+++ JS/LigaDeLaJusticia.js
@@ -0,0 +1,55 @@
+console.log("Funcionando");
+//1.Evento que carga la función cuando el DOM este en la pagina.
+//Se obtiene con Ctrl+space 
+document.addEventListener('DOMContentLoaded', () => {
+
+    Mostrar_LigaDeLaJusticia(Heroes) // Firma cuando carga la pagina
+
+})
+
+//2. Arreglo de Objetos.
+
+const Heroes = [
+    {
+        nombre: "Aquaman",
+        bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
+        img: "img/aquaman.png",
+        casa: "DC"
+      },
+      {
+        nombre: "Batman",
+        bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
+        img: "img/batman.png",
+        casa: "DC"
+      },
+      {
+        nombre: "Superman",
+        bio: "Superman es un superhéroe ficticio que aparece en los cómics publicados por DC Comics. Es uno de los personajes de ficción más populares del siglo XX.",
+        img: "img/superman.png",
+        casa: "DC"
+      },
+      {
+        nombre: "Wonder Woman",
+        bio: "Es una superheroína ficticia creada por el psicólogo y escritor William Moulton Marston para la editorial DC Comics. Sus historias se centran en los ideales del amor, paz y sexualidad.",
+        img: "img/wonder_woman.png",
+        casa: "DC"
+      }
+];
+
+
+
+//3.Función que muestra los Heroes.
+//--A. ID del contenedor.
+//--B.Plantilla.
+//--C. ForEach al paso,, ciclo de la parte 2
+//--D. Meter la plantilla en el contenedor 
+
+function Mostrar_LigaDeLaJusticia(Heroes) {  //Esta es la Firma
+    // let Contenedor //A.
+    // let Plantilla  //B.
+ 
+    // //C.
+    
+    // Contenedor.innerHTML = Plantilla //D.
+   alert(Heroes)
+}
